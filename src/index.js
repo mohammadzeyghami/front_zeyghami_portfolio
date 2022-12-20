5ce5a629550450efee36ad49890f58d4f0184848
mohammadzeyghami <mohammadzeighami197@gmail.com>
2022-12-20 18:36:48 +0100

    assets and profile
    add images in side asets i import all of that in side constans/images.js and aftet that export all inside index.js in there
    now we can use that imges very ez

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import react from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
