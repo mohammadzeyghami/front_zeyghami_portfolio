1627a26537ec88423d94cf2232984b4ab3e4a1b4
mohammadzeyghami <mohammadzeighami197@gmail.com>
2022-12-24 16:28:55 +0100

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        mr.<span>zeyghami</span>
+        mohammad <span>zeyghami</span>
       </div>
       <ul className="app__navbar-links">
         {["home", "about", "work", "skills", "contact"].map((item) => (
